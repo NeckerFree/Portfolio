3ee3d7b594f009d157be47002f88d0579e4d4f67
Elio Cortes <elio.cortes3000@gmail.com>
2022-09-14 19:24:55 -0500

    Created header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import { Container } from 'react-bootstrap';
 import './App.css';
+import OffcanvasNav from './components/navbar/OffcanvasNav';
 
 const App = () => (
   <Container>
-    <h1>Elio Nelson Cortés</h1>
+    <OffcanvasNav />
+    <div className="p-5 text-center bg-image rounded-3 jumbotron">
+      <div className="mask fondo">
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <div className="text-white">
+            <h1 className="mb-3">Elio Cortés</h1>
+            <h4 className="mb-3">Full-Stack Developer</h4>
+            <p className="regard">
+              Hi, I am glad you are visiting my portfolio!
+              I am Elio Cortés — a Bogotá based full-stack engineer.
+              Let me show you my profile.
+            </p>
+            <a className="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </Container>
 );
 export default App;
---
+++ src/components/navbar/OffcanvasNav.js
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import logo from '../../assets/images/logo.png';
+
+function OffcanvasNav() {
+  return (
+    <>
+      <Navbar key="md" bg="light" expand="md" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#"><img className="logoElio" src={logo} alt="logo" /></Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'md'}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${'md'}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${'md'}`}
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'md'}`}>
+                Elio Cortés
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#education">Education</Nav.Link>
+                <Nav.Link href="#experience">Experience</Nav.Link>
+                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+
+    </>
+  );
+}
+
+export default OffcanvasNav;
