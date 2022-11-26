0ca730f6517f7532c151eb69b7c2da0fcb23b4fe
Elio Cortes <elio.cortes3000@gmail.com>
2022-11-25 20:19:45 -0500

    Implemented changes coach

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import './assets/style/App.css';
 import OffcanvasNav from './components/navbar/OffcanvasNav';
-import Jumbotron from './components/Jumbotron';
+// import Jumbotron from './components/Jumbotron';
 import About from './components/About';
-import Education from './components/Education';
-import Skills from './components/Skills';
+// import Education from './components/Education';
+// import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
-import Experiences from './components/Experiences';
+// import Experiences from './components/Experiences';
 import Contact from './components/Contact';
 
 const App = () => (
   <div>
     <OffcanvasNav className="disable-select" />
     <Container className="disable-select text-white flex-column text-center" fluid>
-      <Row><Col><Jumbotron /></Col></Row>
       <Row><Col><About /></Col></Row>
-      <Row><Col><Education /></Col></Row>
-      <Row><Col><Skills /></Col></Row>
+      {/* <Row><Col><Education /></Col></Row> */}
       <Row><Col><Portfolio /></Col></Row>
-      <Row><Col><Experiences /></Col></Row>
+      {/* <Row><Col><Skills /></Col></Row> */}
       <Row><Col><Contact /></Col></Row>
     </Container>
   </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ import {
   Form,
   Row,
 } from 'react-bootstrap';
+// import resume from '../assets/files/Resume Elio Cortés Systems Engineer.pdf';
 
 const Contact = () => {
   const [validated, setValidated] = useState(false);
@@ -27,33 +28,34 @@ const Contact = () => {
       <section className="contact-section">
         <div className="contact-left">
           <h2>Let&apos;s talk</h2>
-          <p>If you have an application you are interested in developing,</p>
-          <p>a feature that you need built or a project that needs coding.</p>
-          <p>I&apos;d love to help with it</p>
-
+          <p>
+            If you have an application you are interested in developing,
+            a project that needs coding I&apos;d love to help with it
+          </p>
           {/* <p>I am open to any collaborations and job opportunities.</p> */}
           <p>elio.cortes3000@gmail.com</p>
+          {/* <a href={resume} className="resume" download="Resume Elio Cortes.pdf">Get my resume</a> */}
           <div className="contact-social">
             <p className="nav justify-content-center">Follow me on:</p>
             <ul className="nav justify-content-center">
               <li className="nav-item">
                 <a className="nav-link" target="_blank" rel="noreferrer" href="https://github.com/NeckerFree">
-                  <img src="./assets/images/social/icons8-github-50.png" alt="github" />
+                  <img src={`${process.env.PUBLIC_URL}/assets/images/social/icons8-github-50.png`} alt="github" />
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/elionelsoncortes">
-                  <img src="./assets/images/social/icons8-linkedin-50.png" alt="linkedin" />
+                  <img src={`${process.env.PUBLIC_URL}/assets/images/social/icons8-linkedin-50.png`} alt="linkedin" />
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" target="_blank" rel="noreferrer" href="https://twitter.com/ElioCortesM">
-                  <img src="./assets/images/social/icons8-twitter-squared-50.png" alt="twitter" />
+                  <img src={`${process.env.PUBLIC_URL}/assets/images/social/icons8-twitter-squared-50.png`} alt="twitter" />
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" target="_blank" rel="noreferrer" href="https://angel.co/u/elio-cortes">
-                  <img src="./assets/images/social/icons8-angellist-50.png" alt="angel" />
+                  <img src={`${process.env.PUBLIC_URL}/assets/images/social/icons8-angellist-50.png`} alt="angel" />
                 </a>
               </li>
             </ul>
@@ -62,7 +64,6 @@ const Contact = () => {
         <Form action="https://formspree.io/f/mrgjqdpe" method="post" noValidate validated={validated} onSubmit={handleSubmit}>
           <Row className="mb-4">
             <Form.Group as={Col} md="8" controlId="fullName">
-              {/* <Form.Label>FullName</Form.Label> */}
               <Form.Control name="name" required type="text" minLength={7} placeholder="Full name" />
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               <Form.Control.Feedback type="invalid">Please enter name (min length 7)</Form.Control.Feedback>
@@ -70,7 +71,6 @@ const Contact = () => {
           </Row>
           <Row className="mb-4">
             <Form.Group as={Col} md="8" controlId="email">
-              {/* <Form.Label>Email</Form.Label> */}
               <Form.Control name="email" required type="email" placeholder="Email" />
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               <Form.Control.Feedback type="invalid">Please enter a valid email</Form.Control.Feedback>
@@ -78,7 +78,6 @@ const Contact = () => {
           </Row>
           <Row className="mb-4">
             <Form.Group as={Col} md="8" controlId="message">
-              {/* <Form.Label>Message</Form.Label> */}
               <Form.Control
                 name="comment"
                 required
@@ -91,7 +90,7 @@ const Contact = () => {
               <Form.Control.Feedback type="invalid">Please enter message (min length 10)</Form.Control.Feedback>
             </Form.Group>
           </Row>
-          <Button className="btn btn-outline-info btn-lg rounded-pill" variant="outline-primary" type="submit">Submit</Button>
+          <Button className="btn btn-light btn-app active" type="submit">Submit</Button>
         </Form>
       </section>
     </Container>
